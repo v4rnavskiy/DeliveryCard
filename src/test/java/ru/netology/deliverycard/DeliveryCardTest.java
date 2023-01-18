54eea7d0ffe8288a87e522904e05bf559d05175f
Evgeniy <varnavskiyev@gmail.com>
2023-01-18 16:32:45 +0300

    added test

---
+++ src/test/java/ru/netology/deliverycard/DeliveryCardTest.java
@@ -0,0 +1,34 @@
+package ru.netology.deliverycard;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Condition.appear;
+import static com.codeborne.selenide.Selenide.*;
+
+public class DeliveryCardTest {
+
+    @Test
+    void shouldOrderCardDeliveryTest() {
+        Configuration.holdBrowserOpen = true;
+        open("http://localhost:9999/");
+        $("[data-test-id='city'] input").setValue("Калуга");
+        String inputDate = LocalDate.now().plusDays(4).format(DateTimeFormatter.ofPattern("dd.MM.uuuu"));
+        SelenideElement data = $("[data-test-id=date]");
+        $("[placeholder='Дата встречи']").doubleClick().sendKeys(Keys.BACK_SPACE);
+        data.$("[placeholder]").setValue(inputDate);
+        $("[placeholder='Город']").sendKeys(Keys.ESCAPE);
+        $("[name='name']").setValue("Петров-Сидоров Иван");
+        $("[name='phone']").setValue("+79999999999");
+        $("[class='checkbox__text']").click();
+        $("[class='button__text']").click();
+        $("[data-test-id='notification']").should(appear, Duration.ofMillis(15000));
+
+    }
+}
